0898e16e979905fb1e835677ce02cc08adfd97b2
muditdahiya <mdtheroxtar@gmail.com>
2023-05-02 12:43:21 -0400

    database connected

--- index.js
+++ index.js
@@ -5,24 +5,39 @@ var mysql = require("mysql");
 // Connect to database
 var con = mysql.createConnection({
   host: "localhost",
-  user: "yourusername",
-  password: "yourpassword",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "cronjob",
 });
-
 con.connect(function (err) {
   if (err) throw err;
   console.log("Connected to database");
 });
 
+function sendBirthdayReminders() {
+  const numbers =
+    "select number from members where Day != day(curdate()) and Month != month(curdate())";
+
+  con.query(numbers, function (err, result) {
+    if (err) throw err;
+    result = JSON.parse(JSON.stringify(result));
+    result.forEach((member) => {
+      console.log(member.number);
+      // Send messages using twilio here
+    });
+  });
+}
+
+sendBirthdayReminders();
+
 // Schedule a daily task
 cron.schedule("0 0 9 * * *", () => {
   // This block will run daily at 9:00AM
+  sendBirthdayReminders();
+});
 
-  // Create the SQL Query
-  const sqlQuery = "";
-
-  con.query(sqlQuery, function (err, result) {
-    if (err) throw err;
-    console.log("Result: " + result);
-  });
+// Schedule a task for every second
+cron.schedule("* * * * * *", () => {
+  // This block will run every second
 });
